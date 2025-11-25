8ee84dda88ee78297cb701b71ddedba2679a651f
Kevin <kevinroysonshp@gmail.com>
2025-11-25 10:48:01 +0700

    Update ChessBoardPanel.java

--- src/main/java/edu/sustech/xiangqi/ui/ChessBoardPanel.java
+++ src/main/java/edu/sustech/xiangqi/ui/ChessBoardPanel.java
@@ -193,6 +193,4 @@ public class ChessBoardPanel extends JPanel {
         g.drawLine(centerX + cornerSize, centerY + cornerSize,
                 centerX + cornerSize, centerY + cornerSize - lineLength);
     }
-    //kontol
-
 }
